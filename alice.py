a7630c1ef7301021943e9d7bb947267a3ca7bf30
kumao <arturleivarx@gmail.com>
2022-08-30 20:34:25 +0200

    alice encrypts hash, session key, file

--- alice.py
+++ alice.py
@@ -152,6 +152,8 @@ with open("key.key", "wb") as key_file:
     key_file.write(key)
 
 ### alice generates hash from file
+from hashlib import blake2b
+
 # divide files in chunks, to not use a lot of ram for big files
 # BUF_SIZE is totally arbitrary, change for your app!
 BUF_SIZE = 65536 # lets read stuff in 64kb chunks!
@@ -166,7 +168,83 @@ with open(FILE, 'rb') as f:
         blake2.update(data)
 
 HASH = "{0}".format(blake2.hexdigest())
-print(HASH)
+
+# write hash to file
+hash_file = open(FILE + ".blake2b", "w")
+n = hash_file.write(HASH)
+hash_file.close()
+
+##### alice encrypts hash w/ alice private key
+
+# Load alice private key
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+with open("alice_private_key.pem", "rb") as key_file:
+    private_key = serialization.load_pem_private_key(
+        key_file.read(),
+        password=None,
+        backend=default_backend()
+    )
+
+# Load the contents of the file to be signed.
+with open(FILE + '.blake2b', 'rb') as f:
+    payload = f.read()
+
+# Sign the payload file.
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
+signature = base64.b64encode(
+    private_key.sign(
+        payload,
+        padding.PSS(
+            mgf = padding.MGF1(hashes.SHA256()),
+            salt_length = padding.PSS.MAX_LENGTH,
+        ),
+        hashes.SHA256(),
+    )
+)
+with open(FILE + '.sig', 'wb') as f:
+    f.write(signature)
+
+##### alice encrypts file w/ session key
+
+# Given a filename (str) and key (bites), it encrypts the file and write it
+f = Fernet(key)
+with open(FILE, "rb") as file:
+    # read all file data
+    file_data = file.read()
+# encrypt data
+encrypted_data = f.encrypt(file_data)
+# write the encrypted file
+with open(FILE + ".encrypted", "wb") as file:
+    file.write(encrypted_data)
+
+##### alice encrypts session key w/ bob public key
+
+# Load bob public key 
+with open("bob_public_key.pem", "rb") as key_file:
+    public_key = serialization.load_pem_public_key(
+        key_file.read(),
+        backend=default_backend()
+    )
+
+# Encrypt file
+data = key
+open('key.encrypted', "wb").close() # clear file
+for encode in data:
+    encrypted = public_key.encrypt(
+        encode,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    
+    with open('key.encrypted', "ab") as f: f.write(encrypted)
+
+##### alice sends encrypted hash, file, session key to bob
  
 ##############
 # close the socket
