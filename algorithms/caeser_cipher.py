d96f10f16151724a3f76f54107c014cda87b0492
kumao <arturleivarx@gmail.com>
2022-10-07 10:53:44 +0200

    added caeser cipher

---
+++ algorithms/caeser_cipher.py
@@ -0,0 +1,20 @@
+#!/bin/env python
+
+plaintext_sample = input('Introdueix el text a codificar: ')
+# Nombre de posicions pel que se substitueix la lletra
+shift_sample = 3
+
+def caeser_encrypt(plaintext, shift):
+    characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    modulo = len(characters)
+    ciphertext = ''
+    
+    for i in range(len(plaintext)):
+        if plaintext[i].isalpha():
+            ciphertext+=characters[((characters.index(plaintext[i].upper())) + shift) % modulo]
+        else:
+            ciphertext += plaintext[i]
+
+    return ciphertext
+
+print(caeser_encrypt(plaintext_sample, shift_sample))
