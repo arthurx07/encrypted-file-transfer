7e06cd19f3331ec0700ed1b676a4c160ae10abee
kumao <arturleivarx@gmail.com>
2022-10-07 10:55:19 +0200

    formatted caeser cipher

--- algorithms/caeser_cipher.py
+++ algorithms/caeser_cipher.py
@@ -5,16 +5,16 @@ plaintext_sample = input('Introdueix el text a codificar: ')
 shift_sample = 3
 
 def caeser_encrypt(plaintext, shift):
-    characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    modulo = len(characters)
-    ciphertext = ''
-    
-    for i in range(len(plaintext)):
-        if plaintext[i].isalpha():
-            ciphertext+=characters[((characters.index(plaintext[i].upper())) + shift) % modulo]
-        else:
-            ciphertext += plaintext[i]
+    characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    modulo = len(characters)
+    ciphertext = ''
 
-    return ciphertext
+    for i in range(len(plaintext)):
+        if plaintext[i].isalpha():
+            ciphertext+=characters[((characters.index(plaintext[i].upper())) + shift) % modulo]
+        else:
+            ciphertext += plaintext[i]
+
+    return ciphertext
 
 print(caeser_encrypt(plaintext_sample, shift_sample))
